23cd33cdebd988118e772cc0b5c65cd8e49bb4cd
Keith Smith <aeiouandsometimesy@me.com>
2017-10-08 14:47:41 -0400

    Initial version tested and working.

--- PRG-8-3-Lottery-Winners-Binary/main.cpp
+++ PRG-8-3-Lottery-Winners-Binary/main.cpp
@@ -5,11 +5,90 @@
 //  Created by Keith Smith on 10/8/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Modify the program you wrote for Programming Challenge 2 (Lottery Winners) so it
+//  performs a binary search instead of a linear search.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+const int INT_ARRAY_SIZE = 8;
+
+int getLotteryWinner();
+bool compareLotteryNumbers(const int[], const int, int);
+
+int main()
+{
+    bool booIsWinner;
+    
+    int intLotteryWinner;
+    int intLotteryChoices[INT_ARRAY_SIZE] = {13579, 26791, 33445, 55555,
+                                             62483, 77777, 85647, 93121};
+    
+    intLotteryWinner = getLotteryWinner();
+    
+    booIsWinner = compareLotteryNumbers(intLotteryChoices, INT_ARRAY_SIZE, intLotteryWinner);
+    
+    if(booIsWinner != true)
+    {
+        cout << "Sorry, your numbers did not match this week's winning number.\n";
+    }
+    else if(booIsWinner == true)
+    {
+        cout << "Congratulations! One of your numbers matched this weeks winning number!\n";
+    }
+    else
+    {
+        cout << "ERROR CHECKING BOOL" << endl;
+    }
+    
     return 0;
 }
+
+int getLotteryWinner()
+{
+    int intWinningNumber;
+    
+    cout << "Please enter the winning lottery number this week: ";
+    cin >> intWinningNumber;
+    while(!intWinningNumber || intWinningNumber < 10000 || intWinningNumber > 99999)
+    {
+        cout << "Please enter a number between 10000 and 99999: ";
+        cin.clear();
+        cin.ignore();
+        cin >> intWinningNumber;
+    }
+    
+    return intWinningNumber;
+}
+
+bool compareLotteryNumbers(const int intArray[], const int INT_SIZE, int intWinner)
+{
+    int first = 0;
+    int last = INT_SIZE - 1;
+    int middle;
+    int position = -1;
+    
+    bool found = false;
+    
+    while(!found && first <= last)
+    {
+        middle = (first + last) / 2;
+        if(intArray[middle] == intWinner)
+        {
+            found = true;
+            position = middle;
+        }
+        else if(intArray[middle] > intWinner)
+        {
+            last = middle - 1;
+        }
+        else
+        {
+            first = middle + 1;
+        }
+    }
+    
+    return found;
+}
+
